daba86d22b1acec8168d87b1fcfb4229f235e66a
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2019-03-07 13:06:52 +0100

    Add protection when attribute already exist

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -4,10 +4,6 @@ namespace MageSuite\CmsProductBacklink\Setup;
 
 class InstallData implements \Magento\Framework\Setup\InstallDataInterface
 {
-    /**
-     * @var \Magento\Eav\Setup\EavSetup
-     */
-    protected $eavSetup;
 
     /**
      * @var \Magento\Eav\Setup\EavSetupFactory
@@ -29,29 +25,31 @@ class InstallData implements \Magento\Framework\Setup\InstallDataInterface
         $setup->startSetup();
 
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        if (!$eavSetup->getAttributeId(\Magento\Catalog\Model\Product::ENTITY, 'cms_pages_ids')) {
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,
+                'cms_pages_ids',
+                [
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                    'type' => 'varchar',
+                    'unique' => false,
+                    'label' => 'Cms Pages Ids',
+                    'input' => 'text',
+                    'source' => '',
+                    'group' => 'General',
+                    'required' => false,
+                    'sort_order' => 10,
+                    'user_defined' => 1,
+                    'searchable' => false,
+                    'filterable' => false,
+                    'filterable_in_search' => false,
+                    'visible_on_front' => false,
+                    'visible' => false,
+                    'used_in_product_listing' => true,
+                    'note' => 'Ids of Cms Pages, where product in displayed'
+                ]
+            );
+        }
 
-        $eavSetup->addAttribute(
-            \Magento\Catalog\Model\Product::ENTITY,
-            'cms_pages_ids',
-            [
-                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
-                'type' => 'varchar',
-                'unique' => false,
-                'label' => 'Cms Pages Ids',
-                'input' => 'text',
-                'source' => '',
-                'group' => 'General',
-                'required' => false,
-                'sort_order' => 10,
-                'user_defined' => 1,
-                'searchable' => false,
-                'filterable' => false,
-                'filterable_in_search' => false,
-                'visible_on_front' => false,
-                'visible' => false,
-                'used_in_product_listing' => true,
-                'note' => 'Ids of Cms Pages, where product in displayed'
-            ]
-        );
     }
 }
